a49fd997d4629eeab900c73b5282d204396b8438
okpalaChidiebere <okpalacollins4@gmail.com>
2022-08-21 08:41:30 -0600

    Starter Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListContactsComponent } from './list-contacts/list-contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListContactsComponent
   ],
   imports: [
     BrowserModule
