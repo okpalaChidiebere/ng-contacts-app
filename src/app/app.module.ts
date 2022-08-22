884fc595c021e1594d031b51e4e28040c8059a0e
okpalaChidiebere <okpalacollins4@gmail.com>
2022-08-21 22:26:23 -0600

    Controlled Component
    - We added the search contact section that allows us to filter ur list using the concept of Forms in angular
    - We bind the input field to a component variable `query` This allows us to update the UI based on the formData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListContactsComponent } from './list-contacts/list-contacts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListContactsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ListContactsComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/list-contacts/list-contacts.component.ts
+++ src/app/list-contacts/list-contacts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 export type Contact = {
   id: string;
@@ -13,13 +13,41 @@ export type Contact = {
   templateUrl: './list-contacts.component.html',
   styleUrls: ['./list-contacts.component.css'],
 })
-export class ListContactsComponent implements OnInit {
+export class ListContactsComponent implements OnInit, OnChanges {
   contacts: Contact[] | null = null;
+  showingContacts: Contact[] | null = null;
   onDeleteContact: (contact: Contact) => void | null;
+  query: string | null = '';
+
   constructor() {}
 
   ngOnInit(): void {
     //verify our contacts props is gotten
     console.log(this.contacts);
+    this.showingContacts = this.contacts;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    /**
+     * Here we listen for when the `contacts` input(props) from the parent
+     * component changes as of the remove button clicked
+     */
+    this.showingContacts = this.contacts;
+    // console.log(changes);
+  }
+
+  updateQueryResult = () => {
+    //this will update the Ui based on the `query` from data
+    this.showingContacts =
+      this.query === ''
+        ? this.contacts
+        : this.contacts.filter((c) =>
+            c.name.toLowerCase().includes(this.query.toLowerCase())
+          );
+  };
+
+  clearQuery() {
+    this.query = '';
+    this.showingContacts = this.contacts;
   }
 }
